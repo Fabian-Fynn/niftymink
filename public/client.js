bdd82f3f6b05236fe389f3791198d53444bc668c
Fabian Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-10-20 01:57:59 +0200

    Feat(images, user): Store username & crt bg image in localStorage Using Handlebars for rendering username

--- public/client.js
+++ public/client.js
@@ -4,7 +4,21 @@ else
   var socket = io.connect('http://niftymink.com', {path: "/public/socket.io"});
 
 $(document).ready(function(){
+  if (localStorage.getItem('current-background')) {
+    $('body').css('background-image', 'url(' + localStorage.getItem('current-background') + ')');
+  }
+
   startTime();
+  renderUsername();
+
+  $('.login-button').click(function(){
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', '/login');
+    form.style.display = 'hidden';
+    document.body.appendChild(form)
+    form.submit();
+  });
 
   socket.on('connected', function(res){
   });
@@ -17,6 +31,7 @@ $(document).ready(function(){
 
   socket.on('newBackground', function(res){
     $('body').css('background-image', 'url(' + res + ')');
+    localStorage.setItem('current-background', res);
   });
 
   socket.on('search-no-results', function() {
@@ -28,4 +43,57 @@ $(document).ready(function(){
       socket.emit('search-request', $(this).val());
     }
   });
+
+  $('#name-field').on('keypress', function(e) {
+    if(e.which == 13) {
+      setUsername($(this));
+      $(this).removeClass('editing');
+      $(this).blur();
+      e.preventDefault();
+    }
+  });
+
+  $('#name-field').on('change', function(e) {
+    setUsername($(this));
+  });
+
+  $('#name-field').on('focusin', function(e) {
+    $(this).addClass('editing');
+    if($(this).hasClass('no-name')) {
+    }
+  });
+
+  $('#name-field').click(function(e) {
+    $(this).addClass('editing');
+    $(this).focus();
+  });
+  $('#name-field').on('focusout blur', function(e) {
+    $(this).removeClass('editing');
+  });
 });
+
+function setUsername($element) {
+  var name = $element.html().replace(/&nbsp;|[-$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|[0-9]/g,'').trim();
+
+  if(name != '')
+    localStorage.setItem('user-name', name);
+  $element.html(localStorage.getItem('user-name'));
+}
+
+function renderUsername() {
+  var context = {
+    username: "Enter name"
+  };
+
+  if (localStorage.getItem('user-name')) {
+    context.username = localStorage.getItem('user-name');
+    var usernameSource = '<span id="name-field" contenteditable="true">{{username}}</span>';
+
+  } else {
+    var usernameSource = '<span id="name-field" class="no-name" contenteditable="true">Enter name</span>';
+  }
+
+  var template = Handlebars.compile(usernameSource);
+  var html = template(context);
+  $('#username').html(html);
+}
